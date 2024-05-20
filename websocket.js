3b3a13b1f7b632289f246ebffe9fa4ce9de1569f
Anthony Chapman <anthony.chapman@designory.com>
2024-05-20 18:59:11 -0400

    add websocketserver

--- websocket.js
+++ websocket.js
@@ -1,10 +1,38 @@
-const express = require('express');
-const server = require('http').createServer();
-const app = express();
+const express = require('express')
+const server = require('http').createServer()
+const app = express()
 
 app.get('/', function(req, res) {
-    res.sendFile('index.html', {root: __dirname});
+    res.sendFile('index.html', {root: __dirname})
 })
 
-server.on('request', app);
-server.listen(3000, function() { console.log('server started on port 3000'); });
+server.on('request', app)
+server.listen(3000, function() { console.log('server started on port 3000'); })
+
+/** Begin websocket */
+const WebSocketServer = require('ws').Server
+
+const wss = new WebSocketServer({server: server})
+
+wss.on('connection', function connection(ws) {
+    const numClients = wss.clients.size;
+    console.log('Clients connected: ', numClients)
+
+    wss.broadcast(`Current visitors: ${numclients}`)
+
+    if (ws.readyState ===  ws.OPEN) {
+        ws.send('Welcome to the demo server')
+    }
+
+    ws.on('close', function close() {
+        wss.broadcast(`Client left, ${numClients} clients remain.`)
+        console.log('A client has disconnected')
+    })
+})
+
+wss.broadast = function broadcast(data) {
+    wss.clients.forEach(function (client) {
+        client.send(data)
+    })
+}
+
